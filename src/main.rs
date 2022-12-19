3bd106b1586736cb9425d3fb5043974e82b827f4
Ask Sødal <asks@uia.no>
2022-12-19 18:49:37 +0100

    counting timer

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,50 @@
 use yew::prelude::*;
+use gloo::timers::callback::{Timeout, Interval};
+use gloo::console;
 
-#[function_component]
-fn App() -> Html {
-    let play = use_state(|| false);
-    let onclick = {
-        let play = play.clone();
-        move |_| {
-            play.set(!*play);
-        }
-    };
+enum Msg{
+    Inc,
+}
+
+struct App{
+    bux: u32,
+    interval: Option<Interval>,
+}
+
+impl Component for App{
+    type Message = Msg;
+    type Properties = ();
     
-    html! {
-        <div styles="color: red;">
-            <button {onclick}>{ "play" }</button>
-            <p>{ *play }</p>
-        </div>
+    fn create(ctx: &Context<Self>) -> Self {
+        
+        let link = ctx.link().clone();
+        Self{
+            bux: 0,
+            interval: Some(Interval::new(1_000, move||link.send_message(Msg::Inc))),
+        }
+    }
+    
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        
+        match msg {
+            Msg::Inc => {
+                if 10 <= self.bux{
+                    self.interval = None
+                } else {
+                    self.bux += 1
+                }
+            }
+        }
+        
+        true
+    }
+    
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <>
+                <h1 id="time">{ self.bux }</h1>
+            </>
+        }
     }
 }
 
